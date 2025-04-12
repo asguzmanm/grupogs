c46e74ee9e71d179d0d9278c7ee8702fff38f408
asguzmanm <85914333+asguzmanm@users.noreply.github.com>
2025-04-12 00:39:30 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,20 @@
 <?php
 date_default_timezone_set("America/Bogota");
 
-$host = "sql312.infinityfree.com";  // El nombre correcto del host de la base de datos
-$user = "if0_38722473";            // Tu usuario de base de datos
-$password = "NqqYSthpmpryPLx";     // Tu contraseña de base de datos
-$db = "if0_38722473_dbgrupogs";    // El nombre exacto de tu base de datos
+// Reemplaza estos valores con las credenciales proporcionadas por Render
+$host = "dpg-cvsvmp49c44c73c7t0cg-a";          // El host de la base de datos de Render
+$user = "dbgrupogs_user";       // El usuario de la base de datos de Render
+$password = "SqNttbLSMe7k32yZZ2BElw6O5Nz2TDo1";// La contraseña de la base de datos de Render
+$db = "dbgrupogs";   // El nombre de la base de datos en Render
 
-try{
-    // Intentamos la conexión con el puerto explícitamente
-    $conn = new PDO("mysql:host=$host;port=3306;dbname=$db", $user, $password);
+try {
+    // Intentamos la conexión con PostgreSQL
+    $conn = new PDO("pgsql:host=$host;dbname=$db", $user, $password);
     // Establecer el modo de error de PDO a excepción
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conexión exitosa";
-}catch(PDOException $e){
-    die("Fallo la conexión: ".$e->getMessage());
+} catch (PDOException $e) {
+    die("Fallo la conexión: " . $e->getMessage());
 }
 ?>
+
