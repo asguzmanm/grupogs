dd293647f8ef7e24e55a59509d6256d4afe5b191
asguzmanm <85914333+asguzmanm@users.noreply.github.com>
2025-04-12 00:23:38 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,13 @@
 <?php
 date_default_timezone_set("America/Bogota");
-//$server="localhost";
-//$user="root";
-//$pass="";
-//$db="inversionesg";
 
-$server="sql312.infinityfree.com";
-$user="if0_38722473";
-$pass="NqqYSthpmpryPLx";
-$db="if0_38722473_dbgrupogs";
+$host = "sql312.infinityfree.com";  // El nombre correcto del host de la base de datos
+$user = "if0_38722473";            // Tu usuario de base de datos
+$password = "NqqYSthpmpryPLx";     // Tu contraseña de base de datos
+$db = "if0_38722473_dbgrupogs";    // El nombre exacto de tu base de datos
 
 try{
-    $conn = new PDO("mysql:host=$server;dbname=$db;",$user,$pass);
+    $conn = new PDO("mysql:host=$host;dbname=$db;",$user,$password);
 }catch(PDOException $e){
     die("Fallo la conexión: ".$e->getMessage());
 }
